7bb29a90f9fe750cfb697f9f14e46fcc49aa99d7
Kirill <kirill.kirzha.work@gmail.com>
2023-12-19 15:30:32 +0300

    feat: initial routing

--- src/ui-kit/FadeAnimation/FadeAnimation.tsx
+++ src/ui-kit/FadeAnimation/FadeAnimation.tsx
@@ -17,7 +17,7 @@ export const FadeAnimation = ({
 }: PropsWithChildren<IProps>) => {
   return (
     <Transition
-      style={fullWidth ? { width: "100%", height: "100%" } : null}
+      style={fullWidth ? { width: "100%", height: "100%" } : undefined}
       className={className}
       key={animationKey}
       as={"div"}
