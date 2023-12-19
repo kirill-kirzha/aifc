eee3c4fc89bfae2e30bac13c5ed62e293477ce33
Kirill <kirill.kirzha.work@gmail.com>
2023-12-20 02:38:05 +0300

    fix: minor fixes

--- src/components/AuthorizationPage/AuthorizationPage.tsx
+++ src/components/AuthorizationPage/AuthorizationPage.tsx
@@ -2,7 +2,6 @@ import {Input} from "../../ui-kit/Input";
 import {AuthorizationLayout, AuthorizationForm, LogoText, AuthorizationTypography} from "./AuthorizationPage.styles";
 import {Button} from "../../ui-kit/Button";
 import {useState} from "react";
-import {useNavigate} from "react-router-dom";
 import {useAppDispatch} from "../../hooks/useAppDispatch";
 import {setUserName} from "../../app/store/userSlice";
 
